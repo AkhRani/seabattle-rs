d0d42b4af57ecbc2f88674bf03ffca8cc869ba38
Kevin Hart <kevin.kevinhart@gmail.com>
2017-12-19 21:33:05 -0800

    Print map

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+const WIDTH: usize = 8;
+const HEIGHT: usize = 8;
+
+struct Position {
+    x: usize,
+    y: usize,
+}
+
+struct Entity {
+    pos: Position,
+}
+
+fn print_map(entities: &Vec<Entity>) {
+    let mut tiles = [["   "; WIDTH]; HEIGHT];
+    for e in entities {
+        tiles[e.pos.x][e.pos.y] = "XXX";
+    }
+
+    println!("{}", ".".repeat(WIDTH*3+2));
+    for y in 0..HEIGHT {
+        print!(".");
+        for x in 0..WIDTH {
+            print!("{}", tiles[x][y]);
+        }
+        println!(".");
+    }
+    println!("{}", ".".repeat(WIDTH*3+2));
+}
+
 fn main() {
     println!("Hello, world!");
+    let mut entities: Vec<Entity> = Vec::new();
+    entities.push(
+        Entity {
+            pos: Position { x: 5, y: 1}
+        });
+    print_map(&entities)
 }
