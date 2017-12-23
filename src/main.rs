b3ecfaf68b6567b6a8bf7eb31a5180c7b8ec1536
Kevin Hart <kevin.kevinhart@gmail.com>
2017-12-23 14:41:56 -0800

    Add entity ID enum

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,31 @@ struct Position {
     y: usize,
 }
 
+enum EType {
+    Player,
+    Island,
+    Ship,
+    Mine,
+    HQ,
+    Monster
+}
+
 struct Entity {
     pos: Position,
+    etype: EType,
 }
 
 fn print_map(entities: &Vec<Entity>) {
     let mut tiles = [["   "; WIDTH]; HEIGHT];
     for e in entities {
-        tiles[e.pos.x][e.pos.y] = "XXX";
+        tiles[e.pos.x][e.pos.y] = match e.etype {
+            EType::Player => "(X)",
+            EType::Island => "***",
+            EType::Ship => "\\S/",
+            EType::Mine => " $ ",
+            EType::HQ => "-H-",
+            EType::Monster => "SSS",
+        }
     }
 
     println!("{}", ".".repeat(WIDTH*3+2));
@@ -27,12 +44,19 @@ fn print_map(entities: &Vec<Entity>) {
     println!("{}", ".".repeat(WIDTH*3+2));
 }
 
-fn main() {
-    println!("Hello, world!");
-    let mut entities: Vec<Entity> = Vec::new();
+fn setup() -> Vec<Entity> {
+    let mut entities = Vec::new();
     entities.push(
         Entity {
-            pos: Position { x: 5, y: 1}
+            pos: Position { x: 5, y: 1},
+            etype: EType::Ship,
         });
-    print_map(&entities)
+    entities
+}
+
+fn main() {
+    println!("Hello, world!");
+    // let mut entities: Vec<Entity> = Vec::new();
+    let entities = setup();
+    print_map(&entities);
 }
